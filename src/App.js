b44ca0ed1a8158faafc411f643d4f3af24192b8d
SamsonGulma <sams0ngultek@gmail.com>
2024-05-15 21:43:52 +0000

    May 16, 2024, 12:43 AM

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Software<br />Engineering <br /> Student <br /> at ASTU 
-
+        <p>
+          Software<br />Engineering <br /> Student <br /> at ASTU 
+        </p>
+          
       </header>
     </div>
   );
