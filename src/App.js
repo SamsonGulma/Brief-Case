68991df811c3ee14c4d02fd98f5828169197460a
SamsonGulma <sams0ngultek@gmail.com>
2024-05-16 23:45:15 +0300

    updates....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import picture from './assets/ipic.jpg'
 import linkedIn from './assets/icons8-linkedin.gif'
-import { useNavigate } from "react-router-dom";
 
 function App() {
-  const navigator = useNavigate();
   return (
     <div className="App">
       <div className="App-header">
@@ -20,7 +18,7 @@ function App() {
       <img className='pic' src={picture} alt=''/>
       <div className='sam'><p>Sam</p></div>
       <img className='linkedin' src={linkedIn} alt='' onClick={() => {
-       navigator('https://www.linkedin.com/')
+      
       }} />
       
 
