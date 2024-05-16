0516b435001a34137dec4301151ac1679c00b5af
SamsonGulma <sams0ngultek@gmail.com>
2024-05-16 20:41:20 +0000

    May 16, 2024, 11:41 PM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import picture from './assets/ipic.jpg'
 import linkedIn from './assets/icons8-linkedin.gif'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigator = useNavigate();
   return (
     <div className="App">
       <div className="App-header">
@@ -18,7 +20,7 @@ function App() {
       <img className='pic' src={picture} alt=''/>
       <div className='sam'><p>Sam</p></div>
       <img className='linkedin' src={linkedIn} alt='' onClick={() => {
-       
+       navigator('https://www.linkedin.com/')
       }} />
       
 
