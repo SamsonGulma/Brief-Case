1eee138841bf23295c2dab3b013f679f25ddb54a
SamsonGulma <sams0ngultek@gmail.com>
2024-05-18 12:44:46 +0000

    May 18, 2024, 3:44 PM

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import linkedIn from './assets/icons8-linkedin.gif'
 function App() {
   return (
     <div className="App">
+      
+      <svg viewBox="0 0 200 200" >
+        <path fill="#D0E2FF" d="M32.5,-46.5C45.3,-35.4,61.1,-30.2,69.3,-19.2C77.4,-8.2,77.9,8.6,72.8,23.4C67.7,38.2,57,50.9,43.9,60.2C30.8,69.5,15.4,75.4,-2,78.1C-19.4,80.9,-38.8,80.5,-48.7,70.2C-58.7,59.9,-59.2,39.6,-61.7,22.3C-64.3,5,-68.8,-9.4,-67,-23.9C-65.2,-38.4,-57.1,-52.9,-44.9,-64.2C-32.6,-75.5,-16.3,-83.5,-3.3,-79.1C9.8,-74.6,19.6,-57.6,32.5,-46.5Z" transform="translate(100 100)" />
+      </svg>
 
       <div className="App-header">
           Software<br />Engineering <br /> Student <br /> at ASTU 
@@ -16,7 +20,6 @@ function App() {
           Hey there, I'm Samson
         </p>
       </div>
-
 
       <img className='pic' src={picture} alt=''/>
       <div className='sam'><p>Sam</p></div>
