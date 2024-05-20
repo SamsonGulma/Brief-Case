8ef4acb1c7178b873d645e2dde0bfcdb443cec43
SamsonGulma <sams0ngultek@gmail.com>
2024-05-20 17:23:52 +0000

    May 20, 2024, 8:23 PM

--- src/App.js
+++ src/App.js
@@ -96,12 +96,12 @@ function App() {
         </div>
 
         <div className='cards'>
-        <div className='cards1'></div>
-        <div className='cards2'></div>
-        <div className='cards3'></div>
-        <div className='cards4'></div>
-        <div className='cards5'></div>
-        <div className='cards6'></div>
+        <div className='card1'></div>
+        <div className='card2'></div>
+        <div className='card3'></div>
+        <div className='card4'></div>
+        <div className='card5'></div>
+        <div className='card6'></div>
         </div>
 
 
