f469eb0bd46167f84cec20a1857ce621e086feff
SamsonGulma <sams0ngultek@gmail.com>
2024-05-20 14:37:15 +0000

    May 20, 2024, 5:37 PM

--- src/components/OnscrollEvents.js
+++ src/components/OnscrollEvents.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+
 const OnscrollEvents = () => {
+    const section1 = document.getElementsByClassName('onFirst');
+
     window.addEventListener('scroll', () => {
         const scrollOnY = window.scrollY;  
         console.log(scrollOnY);
+
+        if (scrollOnY === 20) {
+            section1.style.backgroundColor = 'black';
+        }
     })
 
   return (
