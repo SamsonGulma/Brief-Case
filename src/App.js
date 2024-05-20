7463a0bca6ea0fd76039e02e82b3bfee0b268b22
SamsonGulma <sams0ngultek@gmail.com>
2024-05-20 03:32:36 +0300

    updates...

--- src/App.js
+++ src/App.js
@@ -21,11 +21,7 @@ function App() {
     <div className="App">
 
       <div  className='shape1'></div>
-      <div  className='shape2'></div>
-      <div  className='shape3'></div>
-      <div  className='shape4'></div>
-      <div className='shape5'></div>
-      <div className='shape6'></div>
+      
 
       
     
