d241259958bb777fe2303559cf39a7d26d2b0d37
SamsonGulma <sams0ngultek@gmail.com>
2024-06-21 13:02:47 +0000

    Jun 21, 2024, 4:02 PM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { useEffect } from 'react'
 import './App.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css'
-import picture from './assets/ipic.jpg'
-import linkedIn from './assets/icons8-linkedin.gif'
-import github from './assets/icons8-github-48.png'
 
 
 function App() {
     
-
-  useEffect(() => {
-    AOS.init(
-      {
-        duration: 3000
-      }
-    )
-  }, []
-  );
   return (
     <div className='App'>
 
